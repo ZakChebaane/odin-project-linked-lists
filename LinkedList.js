55531f23d2185eb6bdb2d88dab8a60e0940e4f13
ZakChebaane <150349051+ZakChebaane@users.noreply.github.com>
2026-08-08 18:03:00 +0100

    task1 -> 7 completed

--- LinkedList.js
+++ LinkedList.js
@@ -5,7 +5,7 @@ export class LinkedList {
         this.head = null;
     };
     append(value) {
-        if (this.head === null) {
+        if (!this.head) {
             this.head = new Node(value);
         } else if (!this.head.nextNode) {
             this.head.nextNode = new Node(value);
@@ -23,6 +23,15 @@ export class LinkedList {
             }
         }
     };
+    prepend(value) {
+        if (!this.head) {
+            this.head = new Node(value);
+        } else if (this.head) {
+            const newNode = new Node(value);
+            newNode.nextNode = this.head;
+            this.head = newNode;
+        }
+    };
     size() {
         let returnSize = 0;
         let traverse = true;
@@ -37,5 +46,79 @@ export class LinkedList {
             }
         }
         return returnSize;
+    };
+    getHead() {
+        if (!this.head) {
+            return undefined;
+        } else if (this.head) {
+            return this.head.value;
+        }
+    };
+    getTail() {
+        if (!this.head) {
+            return undefined;
+        } else if (this.head) {
+            let traverse = true;
+            let currNode = this.head;
+            while (traverse) {
+                if (!currNode.nextNode) {
+                    return currNode.value;
+                } else if (currNode.nextNode) {
+                    currNode = currNode.nextNode;
+                }
+            }
+        }
+    }
+    at(index) {
+        if (!this.head) {
+            return undefined;
+        } else if (this.head) {
+            let currNode = this.head;
+            while(index > 0) {
+                if (index === 1) {
+                    return currNode.value;
+                }
+                else if (currNode.nextNode) {
+                    index--;
+                    currNode = currNode.nextNode;
+                } else if (!currNode.nextNode) {
+                    return undefined
+                }
+            }
+        }
+    }
+    pop() {
+        const nextNode = this.head.nextNode;
+        const currNode = this.head;
+        this.head = nextNode;
+        if (currNode) {
+            return currNode.value;
+        } else if (!currNode) {
+            return undefined;
+        }
+    }
+    contains(value) {
+        let currNode = this.head;
+        let traverse = true;
+        if (!this.head) {
+            return false;
+        }
+        while(traverse) {
+            if(currNode.nextNode) {
+                if (currNode.value === value) {
+                    return true;
+                }
+                currNode = currNode.nextNode;
+            } else if (!currNode.nextNode) {
+                if (currNode.value === value) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    findIndex(value) {
+        
     }
 };
--- index.js
+++ index.js
@@ -2,8 +2,18 @@ import {LinkedList} from "./LinkedList.js";
 
 const myLinkedList = new LinkedList();
 
-myLinkedList.append(1);
 myLinkedList.append(2);
 myLinkedList.append(3);
+myLinkedList.append(4);
+myLinkedList.append(5);
+myLinkedList.prepend(1);
 
-console.log(myLinkedList.size());
+console.log("pop -> ", myLinkedList.pop());
+
+console.log(myLinkedList.at(1));
+console.log(myLinkedList.at(2));
+console.log(myLinkedList.at(3));
+console.log(myLinkedList.at(4));
+console.log(myLinkedList.at(5));
+
+console.log(myLinkedList.contains(1000));
